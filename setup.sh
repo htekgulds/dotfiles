bba8c4d967bfaea20e663d325df19de89c48845c
Hasan TEKGÜL <hasan.tekgul@tuik.gov.tr>
2024-03-21 13:38:55 +0300

    update main setup script

--- setup.sh
+++ setup.sh
@@ -1,6 +1,30 @@
-# zsh-syntax-highlighting setup as zsh plugin
-# [ ! -d "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting" ] && git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+# Colors
+RED='\033[0;31m'
+NC='\033[0m' # No Color
 
-[ ! -s "$HOME/.zshrc" ] && ln -s $(pwd)/.zshrc $HOME/.zshrc
-[ ! -s "$HOME/.npmrc" ] && ln -s $(pwd)/.npmrc $HOME/.npmrc
-[ ! -s "$HOME/.p10k.zsh" ] && ln -s $(pwd)/.p10k.zsh $HOME/.p10k.zsh
+# run setup scripts
+chmod +x setup/*.sh
+for script in setup/*.sh; do
+    printf "running ${RED}$script${NC}\n"
+    $(pwd)/$script
+done
+
+# copy dotfiles
+for file in home/*; do
+    name=$(basename $file)
+    if [ ! -s "$HOME/.$name" ]; then
+        printf "copying ${RED}$file${NC} as ${RED}$HOME/.$name${NC}\n"
+        ln -s $(pwd)/$file $HOME/.$name
+    else
+        printf "${RED}$HOME/.$name${NC} already exists, skipping\n"
+    fi
+done
+
+# copy bins
+chmod +x bin/*
+if [ ! -s "$HOME/.local/bin" ]; then
+    printf "copying ${RED}bin${NC} as ${RED}$HOME/.local/bin${NC}\n"
+    ln -s $(pwd)/bin $HOME/.local/bin
+else
+    printf "${RED}$HOME/.$name${NC} already exists, skipping\n"
+fi
